685521214ba2a194e9d831bf21be805f9cb737bf
Troy Gorringe <troy@gorringe.co.nz>
2020-05-21 22:43:14 +1200

    Reorder definitions

--- src/solaxService.ts
+++ src/solaxService.ts
@@ -3,6 +3,36 @@ import _ from 'lodash';
 import { Logger } from 'homebridge/lib/logger';
 import { Config } from './config';
 
+enum LiveDatastreamFields {
+  PV1Current = 1,
+  PV2Current = 2,
+  PV1Voltage = 3,
+  PV2Voltage = 4,
+  GridCurrent = 5,
+  GridVoltage = 6,
+  GridPower = 7,
+  InnerTemp = 8,
+  SolarToday = 9,
+  SolarTotal = 10,
+  FeedInPower = 11,
+  PV1Power = 12,
+  PV2Power = 13,
+  BatteryVoltage = 14,
+  BatteryCurrent = 15,
+  BatteryPower = 16,
+  BatteryTemp = 17,
+  BatteryCapacity = 19,
+  SolarTotal2 = 20,
+  EnergyToGrid = 42,
+  EnergyFromGrid = 43,
+  GridFrequency = 51,
+  EPSVoltage = 54,
+  EPSCurrent = 55,
+  EPSVA = 56,
+  EPSFrequency = 57,
+  Status = 69,
+}
+
 export const getValuesAsync = async (log: Logger, config: Config): Promise<InverterLiveMetrics> => {
   try {
     const response = await fetch(`${config.address}/api/realTimeData.htm`);
@@ -34,34 +64,4 @@ export const getValuesAsync = async (log: Logger, config: Config): Promise<Inver
 export interface InverterLiveMetrics {
     generationWatts: number;
     exportedWatts: number;
-} 
-
-enum LiveDatastreamFields {
-  PV1Current = 1,
-  PV2Current = 2,
-  PV1Voltage = 3,
-  PV2Voltage = 4,
-  GridCurrent = 5,
-  GridVoltage = 6,
-  GridPower = 7,
-  InnerTemp = 8,
-  SolarToday = 9,
-  SolarTotal = 10,
-  FeedInPower = 11,
-  PV1Power = 12,
-  PV2Power = 13,
-  BatteryVoltage = 14,
-  BatteryCurrent = 15,
-  BatteryPower = 16,
-  BatteryTemp = 17,
-  BatteryCapacity = 19,
-  SolarTotal2 = 20,
-  EnergyToGrid = 42,
-  EnergyFromGrid = 43,
-  GridFrequency = 51,
-  EPSVoltage = 54,
-  EPSCurrent = 55,
-  EPSVA = 56,
-  EPSFrequency = 57,
-  Status = 69,
 }
