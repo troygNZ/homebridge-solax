528ededee181242b112c5a67f32db1e8bdea45d0
Troy Gorringe <troy@gorringe.co.nz>
2020-05-25 19:12:48 +1200

    Installation of prettier

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import type { PlatformConfig } from 'homebridge';
+import type { PlatformConfig } from "homebridge";
 
 export default interface Config extends PlatformConfig {
-    address: string;
-    latitude?: number;
-    longitude?: number;
-    exportAlertThresholds?: number[];
+  address: string;
+  latitude?: number;
+  longitude?: number;
+  exportAlertThresholds?: number[];
 }
