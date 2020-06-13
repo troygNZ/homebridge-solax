54f7ab82009a317f281be1bf40c05b4021702ce5
Troy Gorringe <troy@gorringe.co.nz>
2020-06-13 20:54:47 +1200

    Added more explicit response checking, and adding timeout based on polling frequency

--- src/solaxService.ts
+++ src/solaxService.ts
@@ -36,51 +36,56 @@ enum LiveDatastreamFields {
 
 export const getValuesAsync = async (log: Logger, config: Config): Promise<InverterMetrics> => {
   try {
-    const response = await fetch(`${config.address}/api/realTimeData.htm`);
-    const bodyText = await response.text();
-    const cleansedResponse = bodyText.replace(/,,/g, ",0,").replace(/,,/g, ",0,");
-    const json = JSON.parse(cleansedResponse);
-    let genPower: number | null = null;
-    let exportPower: number | null = null;
-    let batteryPercentage: number | null = null;
-    let batteryPowerWatts: number | null = null;
-    let pv1PowerWatts: number | null = null;
-    let pv2PowerWatts: number | null = null;
+    const response = await fetch(`${config.address}/api/realTimeData.htm`, { timeout: config.pollingFrequencySeconds * 1000 });
+    if (response.ok) {
+      const bodyText = await response.text();
+      const cleansedResponse = bodyText.replace(/,,/g, ",0,").replace(/,,/g, ",0,");
+      const json = JSON.parse(cleansedResponse);
+      let genPower: number | null = null;
+      let exportPower: number | null = null;
+      let batteryPercentage: number | null = null;
+      let batteryPowerWatts: number | null = null;
+      let pv1PowerWatts: number | null = null;
+      let pv2PowerWatts: number | null = null;
 
-    _.each(json.Data, (dataItem, index: number) => {
-      // Lookup index data provided starts at 1 :|
-      const lookupIndex = index + 1;
-      if (lookupIndex === (LiveDatastreamFields.GridPower as number)) {
-        genPower = parseInt(dataItem);
-      } else if (lookupIndex === (LiveDatastreamFields.FeedInPower as number)) {
-        exportPower = parseInt(dataItem);
-      } else if (lookupIndex === (LiveDatastreamFields.BatteryCapacity as number)) {
-        batteryPercentage = parseInt(dataItem);
-      } else if (lookupIndex === (LiveDatastreamFields.BatteryPower as number)) {
-        batteryPowerWatts = parseInt(dataItem);
-      } else if (lookupIndex === (LiveDatastreamFields.PV1Power as number)) {
-        pv1PowerWatts = parseInt(dataItem);
-      } else if (lookupIndex === (LiveDatastreamFields.PV2Power as number)) {
-        pv2PowerWatts = parseInt(dataItem);
-      }
-    });
+      _.each(json.Data, (dataItem, index: number) => {
+        // Lookup index data provided starts at 1 :|
+        const lookupIndex = index + 1;
+        if (lookupIndex === (LiveDatastreamFields.GridPower as number)) {
+          genPower = parseInt(dataItem);
+        } else if (lookupIndex === (LiveDatastreamFields.FeedInPower as number)) {
+          exportPower = parseInt(dataItem);
+        } else if (lookupIndex === (LiveDatastreamFields.BatteryCapacity as number)) {
+          batteryPercentage = parseInt(dataItem);
+        } else if (lookupIndex === (LiveDatastreamFields.BatteryPower as number)) {
+          batteryPowerWatts = parseInt(dataItem);
+        } else if (lookupIndex === (LiveDatastreamFields.PV1Power as number)) {
+          pv1PowerWatts = parseInt(dataItem);
+        } else if (lookupIndex === (LiveDatastreamFields.PV2Power as number)) {
+          pv2PowerWatts = parseInt(dataItem);
+        }
+      });
 
-    log.debug("Power Gen: " + genPower);
-    log.debug("Export: " + exportPower);
-    log.debug("Battery Percentage: " + batteryPercentage);
-    log.debug("Battery Power: " + batteryPowerWatts);
-    log.debug("PV1: " + pv1PowerWatts);
-    log.debug("PV2: " + pv2PowerWatts);
+      log.debug("Power Gen: " + genPower);
+      log.debug("Export: " + exportPower);
+      log.debug("Battery Percentage: " + batteryPercentage);
+      log.debug("Battery Power: " + batteryPowerWatts);
+      log.debug("PV1: " + pv1PowerWatts);
+      log.debug("PV2: " + pv2PowerWatts);
 
-    return {
-      timestamp: new Date(),
-      generationWatts: genPower ?? 0,
-      exportedWatts: exportPower ?? 0,
-      batteryPercentage: batteryPercentage ?? 0,
-      batteryPowerWatts: batteryPowerWatts ?? 0,
-      pv1PowerWatts: pv1PowerWatts ?? 0,
-      pv2PowerWatts: pv2PowerWatts ?? 0,
-    };
+      return {
+        timestamp: new Date(),
+        generationWatts: genPower ?? 0,
+        exportedWatts: exportPower ?? 0,
+        batteryPercentage: batteryPercentage ?? 0,
+        batteryPowerWatts: batteryPowerWatts ?? 0,
+        pv1PowerWatts: pv1PowerWatts ?? 0,
+        pv2PowerWatts: pv2PowerWatts ?? 0,
+      };
+    } else {
+      log.error("Failed call to solaxInverter");
+      throw Error(response.statusText);
+    }
   } catch (error) {
     log.error(`That did not go well. Error: ${error}`);
     throw error;
